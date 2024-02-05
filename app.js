a966cff65e43a6cb53448ca806209410eaaefafd
Obskyur <parkourman.miller@gmail.com>
2024-02-04 21:21:51 -0800

    init mongoose db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const { PORT = 3001 } = process.env;
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
 app.listen(PORT);
