a6fcf3e3b4bd4951b7674062bb5c61ff0d183e85
Obskyur <parkourman.miller@gmail.com>
2024-02-04 21:49:48 -0800

    undo rf: use imports. Didn't work.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Imports:
-import express from 'express';
-import { connect } from 'mongoose';
+const express = require('express');
+const { connect } = require('mongoose');
 
 // Vars:
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
-app.listen(PORT);
+connect('mongodb://127.0.0.1:27017/wtwr_db');
+app.listen(PORT, () => {
+  console.log(`\nServer running on port ${PORT}`);
+});
