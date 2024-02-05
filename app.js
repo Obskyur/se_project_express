2d9895995091a2503bd91a76bbf4cb8ecc1cda11
Obskyur <parkourman.miller@gmail.com>
2024-02-04 21:50:42 -0800

    docs: add comments

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const { connect } = require('mongoose');
 const app = express();
 const { PORT = 3001 } = process.env;
 
+// Connect to database:
 connect('mongodb://127.0.0.1:27017/wtwr_db');
+
+// Begin listening for requests:
 app.listen(PORT, () => {
   console.log(`\nServer running on port ${PORT}`);
 });
