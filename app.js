f549613575e69d1d04301b5fd4f08524e8eb1fab
Obskyur <parkourman.miller@gmail.com>
2024-02-04 21:46:00 -0800

    rf: use import statements instead of require()

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const { PORT = 3001 } = process.env;
-const express = require('express');
-const mongoose = require('mongoose');
+// Imports:
+import express from 'express';
+import { connect } from 'mongoose';
+
+// Vars:
 const app = express();
+const { PORT = 3001 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
 app.listen(PORT);
