84fa22543bd2fd653e171e992d511c5d8d2f885c
Obskyur <parkourman.miller@gmail.com>
2024-02-05 21:05:39 -0800

    feat: Add schemas

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,1 +1,45 @@
+// Imports:
 const validator = require('validator');
+const { Schema, model } = require('mongoose');
+
+// Constants:
+const clothingItemSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "The name field is required."],
+    minlength: 2,
+    maxlength: 30
+  },
+  weather: {
+    type: String,
+    required: [true, "The weather field is required."],
+    enum: ['hot', 'warm', 'cold'],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "The imageUrl field is required."],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL.",
+    }
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: [true, "The owner field is required."],
+    ref: 'owner'
+  },
+  likes: {
+    type: Schema.Types.ObjectId,
+    required: [true, "The likes field is required."],
+    ref: 'user'
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+})
+
+module.exports = model("clothingItem", clothingItemSchema);
--- models/user.js
+++ models/user.js
@@ -1,1 +1,26 @@
+// Imports:
 const validator = require('validator');
+const { Schema, model } = require('mongoose');
+
+// Constants:
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "The name field is required."],
+    minlength: 2,
+    maxlength: 30
+  },
+  avatar: {
+    type: String,
+    required: [true, "The avatar field is required."],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL.",
+    }
+  }
+})
+
+// Exports:
+module.exports = model("user", userSchema);
