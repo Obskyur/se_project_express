94773e923a78f38fe3b8c12f0b30ff012b2e3cc1
Obskyur <parkourman.miller@gmail.com>
2024-02-05 22:16:23 -0800

    feat: Add getUser error handling
    TODO: ClothingItems controllers

--- controllers/users.js
+++ controllers/users.js
@@ -19,11 +19,16 @@ const getUsers = (req, res) => {
 const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
+    .orFail()
     .then(users =>
       res.status(200).send(users)
     )
     .catch(err => {
       console.error(err);
+      if (err.name == "DocumentNotFoundError")
+        return res.status(400).send({ message: err.message });
+      else if (err.name == "CastError")
+        return res.status(404).send({ message: err.message });
       return res.status(err.status).send({ message: err.message });
     });
 };
