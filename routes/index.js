03c6e53f8dc63bd02b05b3b1f4e401c778684b14
Obskyur <parkourman.miller@gmail.com>
2024-02-06 12:12:13 -0800

    feat: Add response for non-existent resources

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const clothingItemsRouter = require('./clothingItems');
 //* Re-routing:
 router.use('/users', userRouter);
 router.use('/items', clothingItemsRouter);
+router.use((req, res) =>
+  res.status(500).send({ message: "Requested resource not found." })
+);
 
 //* Exports:
 module.exports = router;
