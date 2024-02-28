57c134a1fdfba7184fd012cf6adc56edc8fa6687
Obskyur <parkourman.miller@gmail.com>
2024-02-27 20:50:16 -0800

    feat: Bind routes to controllers for login / getUser / addUser

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 //* Imports:
 const router = require("express").Router();
-const userRouter = require("./users");
+// const userRouter = require("./users");
+const { getUser, addUser, login } = require("../controllers/users");
 const clothingItemsRouter = require("./clothingItems");
 const { PATH_NOT_FOUND_ERROR } = require('../utils/errors');
 
 //* Re-routing:
-router.use("/users", userRouter);
+router.use("/:id", getUser);
+router.use("/signin", login);
+router.use("/signup", addUser);
 router.use("/items", clothingItemsRouter);
 router.use((req, res) =>
   res.status(PATH_NOT_FOUND_ERROR).send({ message: "Requested resource not found." }),
