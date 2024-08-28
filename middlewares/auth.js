5723148a6c3642e3bee5c8abcc603bece4bf1c47
Obskyur <parkourman.miller@gmail.com>
2024-08-28 12:19:21 -0700

    rf: centralize error handling

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,19 @@
 //* Imports:
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { AUTH_NOT_FOUND_ERROR } = require("../utils/errors");
+const { AUTH_NOT_FOUND_ERROR, UnauthorizedError } = require("../utils/errors");
 
 /* eslint-disable consistent-return */
 module.exports.authorize = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith("Bearer"))
-    return res.status(AUTH_NOT_FOUND_ERROR).send({ message: "Authorization required." });
+    next(new UnauthorizedError("Authorization required"));
   const token = authorization.replace("Bearer ", "");
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(AUTH_NOT_FOUND_ERROR).send({ message: "Authorization required" });
+    next(new UnauthorizedError(err.message));
   }
   req.user = payload;
   next();
